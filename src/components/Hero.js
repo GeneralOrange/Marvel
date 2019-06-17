6e81c6feee253591b957efa0a003e67865aeb159
General Orange <erik.17021995@gmail.com>
2019-06-17 18:49:34 +0200

    Update powers

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react'
 import HeroStatsList from './HeroStatsList'
 
 export default class Hero extends Component {
+	showPowers = (e) => {
+		
+	}
+
     render() {
         return (
             <div className="col-md-3">
-            	<div className="hero">
+            	<div className="hero" id={this.props.hero.id} onClick={this.showPowers}>
 	            	<img className="hero__image" src={this.props.hero.image.url} alt={this.props.hero.name} />
 	                <h3 className="hero__name">{this.props.hero.name}</h3>
 
 	                <table className="hero__stats">
-	                	<HeroStatsList stats={this.props.hero.powerstats}/>
+						<tbody>
+							<HeroStatsList stats={this.props.hero.powerstats}/>
+						</tbody>	
 	                </table>
 	            </div>
             </div>
